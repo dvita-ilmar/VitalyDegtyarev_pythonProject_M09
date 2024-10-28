bf55744c232157d43b49ec2ab74c842dce1bc6a5
dvita-ilmar <dvita@mail.ru>
2024-10-28 12:40:25 +0500

    module_9_3.py is completed

---
+++ module_9_3.py
@@ -0,0 +1,20 @@
+"""
+Дегтярев Виталий (группа 22/08)
+Домашнее задание №9.3
+Домашнее задание по теме "Генераторные сборки"
+"""
+
+
+# Запуск
+if __name__ == '__main__':
+    # Исходные данные: (количество значений в списках должно быть одинаково)
+    first = ['Strings', 'Student', 'Computers']
+    second = ['Строка', 'Урбан', 'Компьютер']
+
+    # Генераторные сборки
+    first_result = (abs(len(i)-len(j)) for i, j in zip(first, second) if len(i) != len(j))
+    second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+    # Вывод результатов
+    print(list(first_result))
+    print(list(second_result))
